442a5e0d9ade5d04ae1cacec63a3fe707ff7c13b
Tomás <141290253+c4usa@users.noreply.github.com>
2025-05-07 20:54:46 -0500

    Update Tallersito.c

--- Taller/Tallersito.c
+++ Taller/Tallersito.c
@@ -9,26 +9,23 @@ int main()
     {
         printf("\nMenú de Opciones:\n");
         printf("\t1. Ingresar producto\n");
-        printf("\t2. Calcular precio total del inventario\n");
-        printf("\t3. Mostrar producto más caro y más barato\n");
+        printf("\t2. Mostrar producto más caro y más barato\n");
+        printf("\t3. Mostrar datos de los productos\n");
         printf("\t4. Mostrar precio promedio\n");
         printf("\t5. Buscar producto por nombre\n");
         printf("\t6. Salir\n");
         printf("Seleccione una opción: ");
-
         opcion = validar(1, 6);
-
         switch (opcion)
         {
         case 1:
             ingresarProducto();
             break;
         case 2:
-            mostrarPrecioTotal();
-            break;
-        case 3:
             mostrarMaxMin();
             break;
+        case 3:
+            mostrarProductos();
         case 4:
             mostrarPromedio();
             break;
